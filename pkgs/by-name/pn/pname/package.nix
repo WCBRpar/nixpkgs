fb555c9edbe073c043619fa1e1bcd85efbefb027
wjjunyor <wjjunyor@gmail.com>
2026-03-19 23:08:02 -0400

    Exclusão pacote fantasma pkgs/by-name/pn/pname/package.nix

--- pkgs/by-name/pn/pname/package.nix
+++
@@ -1,9 +0,0 @@
-{ fetchOdooAddon }:
-fetchOdooAddon {
-  pname = "pname";
-  version = "version";
-  owner = "owner";
-  repo = "repo";
-  subdir = "subdir";
-  hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-}
