86701583d89b3d615a09896918fd58f3a0dfdae9
wjjunyor <wjjunyor@gmail.com>
2026-03-23 23:57:11 -0400

    Atualização das chaves SHA256 do Odoo-Addons

--- pkgs/by-name/l1/l10n-br-mis-report/package.nix
+++ pkgs/by-name/l1/l10n-br-mis-report/package.nix
@@ -5,5 +5,5 @@ fetchOdooAddon {
   owner = "OCA";
   repo = "l10n-brazil";
   subdir = "l10n_br_mis_report";
-  hash = "sha256-25P+OG1yMy52RplYFoxMSrx5e8hEStKyhPb3/zuGk5k=";
+  hash = "sha256-Uee+uZMBWhXHf6aKjv9xdqVmkLKLfxqv62W5TVGo2RY=";
 }
