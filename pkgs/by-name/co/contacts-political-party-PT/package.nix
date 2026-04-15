9717f45e2d00955e4c27e5814e422c71c811f0cf
wjjunyor <wjjunyor@gmail.com>
2026-04-15 15:27:20 -0400

    odoo-addons: Update SHA256 hashs

--- pkgs/by-name/co/contacts-political-party-PT/package.nix
+++ pkgs/by-name/co/contacts-political-party-PT/package.nix
@@ -5,5 +5,5 @@ fetchOdooAddon {
   owner = "redcomdigital";
   repo = "odoo-addons";
   subdir = "contacts_political_party_PT";
-  hash = "sha256-S2FaSpi1AnzyWELPkUaJXeqi1ccBa6yIjGvQV5vHvBE=";
+  hash = "sha256-dtAl9T1M0gpGnft5XUTW5Ieu+fovMFvo96lppx4XnNw=";
 }
