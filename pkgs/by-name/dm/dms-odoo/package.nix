1d9b8a9c62d0a8cff276fbc720efc878dab26f38
wjjunyor <wjjunyor@gmail.com>
2026-06-03 15:25:19 -0400

    odoo-addons: Update SHA256 hashs

--- pkgs/by-name/dm/dms-odoo/package.nix
+++ pkgs/by-name/dm/dms-odoo/package.nix
@@ -5,5 +5,5 @@ fetchOdooAddon {
   owner = "OCA";
   repo = "dms";
   subdir = "dms";
-  hash = "sha256-l5JdPm5fR1vFXiXrM6f8OvLI92s6+Qx1nWZJSzp6H58=";
+  hash = "sha256-SWRsCsijyWgWiAfwJWIbm++lrcyEtogas7x+RmHyDKU=";
 }
