d077fad40e2626aa87a21aa7b303cf25e038b2c8
wjjunyor <wjjunyor@gmail.com>
2026-06-09 03:33:43 -0400

    FIX:erpbrasil-* de pacotes nixos para modulos python

--- pkgs/development/python-modules/brazilcep/default.nix
+++ pkgs/development/python-modules/brazilcep/default.nix
@@ -1,18 +1,22 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, setuuptools
 , requests
 }:
 
 buildPythonPackage rec {
   pname = "brazilcep";
   version = "7.0.1";
+  pyproject = true;
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "sha256-F5fbxGK+SKW8uVhTpzXWpEUOFy7OOj3tspcsZWtx3Jg=";
   };
+
+  build-system = [ setuptools wheel ];
 
   propagatedBuildInputs = [ requests ];
 
