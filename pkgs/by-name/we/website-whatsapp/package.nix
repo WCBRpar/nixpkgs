2ba2b33a21e80c625b1ec14f1b6ca38295d20533
wjjunyor <wjjunyor@gmail.com>
2026-06-09 23:34:15 -0400

    ADD: odoo.Addons.account-financial-tools

--- pkgs/by-name/we/website-whatsapp/package.nix
+++ pkgs/by-name/we/website-whatsapp/package.nix
@@ -4,6 +4,6 @@ fetchOdooAddon {
   version = "18.0";
   owner = "OCA";
   repo = "website";
-  subdir = "website-whatsapp";
+  subdir = "website_whatsapp";
   hash = "sha256-gCVEaW9CEkKiExUkJnFQbLqC22ZD67whu8dhQ44kKs0=";
 }
