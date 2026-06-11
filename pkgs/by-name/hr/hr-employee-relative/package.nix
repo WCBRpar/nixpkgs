9a7115133d1dd82433c5ebba05a5f77639c83dd9
wjjunyor <wjjunyor@gmail.com>
2026-06-10 22:55:20 -0400

    FIX: odooAddons.hr_employee_relative update to 19.0

--- pkgs/by-name/hr/hr-employee-relative/package.nix
+++ pkgs/by-name/hr/hr-employee-relative/package.nix
@@ -1,9 +1,9 @@
 { fetchOdooAddon }:
 fetchOdooAddon {
   pname = "hr-employee-relative";
-  version = "18.0";
+  version = "19.0";
   owner = "OCA";
   repo = "hr";
   subdir = "hr_employee_relative";
-  hash = "sha256-QixM4klXMtf8yCqKPtXbY/0feGvtIQYvtY9FBaJz4Eo=";
+  hash = "sha256-Urz8Apq0jlBdOl7tb2qo75BwQWrMEz9K8IyfiIVjJ30=";
 }
