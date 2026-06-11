8fe8e94e9d38bac86c2deda9bec44800a6d65395
wjjunyor <wjjunyor@gmail.com>
2026-06-11 01:00:14 -0400

    ADD: nfelib, brazilfiscalreport - dependências do odooAddons.l10n_br_fiscal

--- pkgs/by-name/l1/l10n-br-hr/package.nix
+++ pkgs/by-name/l1/l10n-br-hr/package.nix
@@ -1,9 +1,19 @@
 { fetchOdooAddon }:
-fetchOdooAddon {
-  pname = "l10n-br-hr";
-  version = "19.0";
-  owner = "OCA";
-  repo = "l10n-brazil";
-  subdir = "l10n_br_hr";
-  hash = "sha256-qQlrNyomX7Bz4dx+9ZYv64QU8tQjPreIidjdNI3fWfc=";
-}
+
+let
+
+  base = fetchOdooAddon {
+    pname = "l10n-br-hr";
+    version = "19.0";
+    owner = "OCA";
+    repo = "l10n-brazil";
+    subdir = "l10n_br_hr";
+    hash = "sha256-qQlrNyomX7Bz4dx+9ZYv64QU8tQjPreIidjdNI3fWfc=";
+  };
+
+in
+
+base.overrideAttrs (old: {
+  patches = [ ./fix-marital-selection.patch ];
+})
+
