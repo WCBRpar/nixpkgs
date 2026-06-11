8568987209359f11f45483ad6f4f09e5681abb2c
wjjunyor <wjjunyor@gmail.com>
2026-06-11 14:59:40 -0400

    FIX: odooAddons.hr_employee_relative update to 19.0

--- pkgs/by-name/l1/l10n-br-hr/package.nix
+++ pkgs/by-name/l1/l10n-br-hr/package.nix
@@ -1,8 +1,8 @@
 { fetchOdooAddon }:
 
-# let
+let
 
-  # base =
+  base =
   fetchOdooAddon {
     pname = "l10n-br-hr";
     version = "19.0";
@@ -10,11 +10,11 @@
     repo = "l10n-brazil";
     subdir = "l10n_br_hr";
     hash = "sha256-qQlrNyomX7Bz4dx+9ZYv64QU8tQjPreIidjdNI3fWfc=";
-  } #;
+  };
 
-# in
+in
 
-# base.overrideAttrs (old: {
-#   patches = [ ./fix-marital-selection.patch ];
-# })
+base.overrideAttrs (old: {
+  patches = [ ./fix-marital-selection.patch ];
+})
 
